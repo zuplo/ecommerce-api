cc49a97e1c4204491dfe17a267f81bed6c9251a5
Nate Totten <nate@zuplo.com>
2022-02-07 13:10:57 -0500

    initial source

---
+++ README.md
@@ -0,0 +1,9 @@
+# Placeholder API
+
+This is a simple API that returns JSON data for the following.
+
+```
+GET: /v1/users
+GET: /v1/products
+GET: /v1/transactions
+```
---
+++ modules/transactions.ts
@@ -0,0 +1,15 @@
+import { ResponseFactory, ZuploContext, ZuploRequest } from "@zuplo/runtime";
+import data from "./transactions.json";
+import { ApiResponses, ApiResponseFactory } from "./utils/api-responses";
+
+export async function getAll(request: ZuploRequest, context: ZuploContext) {
+  return ApiResponseFactory.formattedResponse(data);
+}
+
+export async function getById(request: ZuploRequest, context: ZuploContext) {
+  const result = data.find(({ id }) => id.toString() === request.params.id);
+  if (!result) {
+    return ResponseFactory.notFound(ApiResponses.NOT_FOUND);
+  }
+  return ApiResponseFactory.formattedResponse(result);
+}
